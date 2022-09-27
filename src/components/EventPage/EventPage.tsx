69c557268013f6ca5757d05af1288a3c73a8a497
Swazi Kunene <kuneneswazi18@gmail.com>
2022-09-27 15:44:26 +0200

    almost done

--- src/components/EventPage/EventPage.tsx
+++ src/components/EventPage/EventPage.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './EventPage.scss';
 
+import { BsCalendar2DateFill } from 'react-icons/bs';
+import { MdArrowForwardIos } from 'react-icons/md';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+
 export const EventPage: React.FC = () => {
   return (
     <div className="event-page">
@@ -16,7 +20,56 @@ export const EventPage: React.FC = () => {
           <p className="event-page__host">
             Hosted by <span className="event-page__host-name">Elysia</span>
           </p>
+
+          <div className="event-page__details">
+            <div className="event-page__detail">
+              <div className="event-page__detail-image">
+                <BsCalendar2DateFill />
+              </div>
+
+              <div className="event-page__detail-content">
+                <div>
+                  <h3 className="event-page__detail-heading">
+                    18 August 6:00PM
+                  </h3>
+
+                  <p className="event-page__detail-subheading">
+                    to
+                    <span className="event-page__detail-subheading--strong"> 19 August 1:00PM </span> 
+                    UTC +10
+                  </p>
+                </div>
+        
+                <MdArrowForwardIos />
+              </div>
+            </div>
+            <div className="event-page__detail">
+              <div className="event-page__detail-image">
+                <HiOutlineLocationMarker />
+              </div>
+
+              <div className="event-page__detail-content">
+                <div>
+                  <h3 className="event-page__detail-heading">
+                    Street name
+                  </h3>
+
+                  <p className="event-page__detail-subheading">
+                    Suburb, State, Postcode
+                  </p>
+                </div>
+        
+                <MdArrowForwardIos />
+              </div>
+            </div>
+          </div>
         </div>
+
+        <img 
+          src={`${process.env.PUBLIC_URL}/images/birthday-cake.png`}
+          alt="birthday cake"
+          className="event-page__image event-page__image--desktop"
+        />
       </div>
     </div>
   );
